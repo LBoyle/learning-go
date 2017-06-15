e07770414980299373321bdcd5ef302af71478d7
LBoyle <louis_boyle@hotmail.com>
2017-06-15 22:01:55 +0100

    made show route

--- first-api/main.go
+++ first-api/main.go
@@ -23,7 +23,14 @@ type Address struct {
 var people []Person
 
 func GetPerson(w http.ResponseWriter, req *http.Request) {
-
+  params := mux.Vars(req)
+  for _, item := range people {
+    if item.ID == params["id"] {
+      json.NewEncoder(w).Encode(item)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(&Person{})
 }
 
 func GetPeople(w http.ResponseWriter, req *http.Request) {
@@ -42,7 +49,7 @@ func main() {
   router := mux.NewRouter()
 
   people = append(people, Person{ID: "1", Firstname: "Louis", Lastname: "Boyle", Address: &Address{City: "London", Borough: "Islington"}})
-  people = append(people, Person{ID: "1", Firstname: "Charlie", Lastname: "Boyle", Address: &Address{City: "London", Borough: "Bow"}})
+  people = append(people, Person{ID: "2", Firstname: "Charlie", Lastname: "Boyle", Address: &Address{City: "London", Borough: "Bow"}})
 
   router.HandleFunc("/people", GetPeople).Methods("GET")
   router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
