210cb1c886572dd4a54ddf3462b58776d34ea7c3
LBoyle <louis_boyle@hotmail.com>
2017-06-15 22:48:42 +0100

    made a connection, seeding two records

--- nosql-db/main.go
+++ nosql-db/main.go
@@ -1,9 +1,34 @@
 package main
 
-include (
 
+import (
+  "fmt"
+	"log"
+  "gopkg.in/mgo.v2"
+  "gopkg.in/mgo.v2/bson"
 )
 
-func main() {
+type Person struct {
+  Name string
+  Email string
+  Address *Address
+}
 
+type Address struct {
+  City string
+  Borough string
+}
+
+func main() {
+  // connect to database
+  session, err := mgo.Dial("mongodb://localhost:27017")
+  // panic if there's an error
+  if err != nil {
+    panic(err)
+  }
+  // don't know what this means
+  defer session.Close()
+  // c for connection? is my guess
+  c := session.DB("testgomongo").C("people")
+  err = c.Insert(&Person{"Louis", "louis@louis.com", {"London", "Islington"}}, &Person{"Gabe", "gabe@gabe.com", {"London", "Islington"}})
 }
